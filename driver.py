6dcc52fbf3c851cc55d7ff56573a7ef75ad8281c
Sarthak67 <73450843+smishra677@users.noreply.github.com>
2021-10-29 15:54:42 -0500

    Added Regraft
    Added Regrafting

--- driver.py
+++ driver.py
@@ -48,7 +48,7 @@ def get_polytomies(input_):
 	    		dictionary_[node].append(children)
 
 	#pprint.pprint(dictionary_)
-	return dictionary_
+	return dictionary_,tree
 
 
 
@@ -72,8 +72,8 @@ def get_sequences(input_,dictionary_):
 	for j in dictionary_.keys():
 		for k in dictionary_[j]:
 			if str(k.taxon) !='None':
-				tax=str(k.taxon)[1:-1]
-				dictionary_output[tax]=dictionary_input[tax]
+				tax=str(k.taxon).strip('"').strip("'")
+				dictionary_output[k]=dictionary_input[tax]
 			
 	
 	return dictionary_output
@@ -84,13 +84,42 @@ def get_sequences(input_,dictionary_):
 def write_fatsa(input_dictionary):
 
 	with open('filtered.fasta', "w+") as handle:
-		print(SeqIO.write(input_dictionary.values(),handle,'fasta'))
+		SeqIO.write(input_dictionary.values(),handle,'fasta')
 
 
-'''
-TODO add the resolved portion of the Tree back to the main Tree)
-def regraft(tree,dictionary_):
-'''
+#TODO add the resolved portion of the Tree back to the main Tree)
+def regraft(tree_,dictionary_):
+	tax = TaxonNamespace()
+	tree = Tree.get(file=open('out_svd.tre', 'r'),
+		            schema="newick",
+		            tree_offset=0,
+		            taxon_namespace=tax,
+		            preserve_underscores=True)
+
+
+	children_to_prune=[]
+	children_to_prune_tree_id=[]
+	for i in tree.postorder_node_iter():
+		if str(i.taxon) !='None':
+			tax=str(i.taxon).strip('"').strip("'")
+			children_to_prune.append(tax)
+
+
+	tree_.prune_taxa_with_labels(children_to_prune)
+
+	node_=list(dictionary_.keys())
+	for node in tree_.postorder_node_iter():
+		if node in node_:
+			node.add_child(tree)
+
+	f=open('out_final.newick','w+')
+	f.write(str(tree_))
+	f.close()
+
+
+
+
+
 
 
 '''
@@ -103,24 +132,23 @@ TODO Compare the tree and generate stats
 #raxml('test.fasta','output_raxaml.tre')
 
 
-astral('RAxML_result.output_raxaml.tre', 'astral.tre')
+#astral('RAxML_result.output_raxaml.tre', 'astral.tre')
 
-branch_collapse('astral.tre','0.1','collapsed.tre')
-dict0=get_polytomies('collapsed.tre')
+branch_collapse('out_try.tree','1','collapsed.tre')
+dict0,tree_=get_polytomies('collapsed.tre')
 #pprint.pprint(dictn)
 dict1=select_polytomies(dict0,5)
-
+#pprint.pprint(dict1)
 for i in dict1.keys():
 	send={}
 	send[i]=dict1[i]
-	print(len(dict1[i]))
 	if len(dict1[i])>=4:
-		print(send)
-		dict2= get_sequences('test.fasta',send)
-		print(dict2)
-		write_fatsa(dict2)
-		convert_fatsa_nexus('filtered.fasta','output.nex')
-		run_svd()
-		#regraft(tree,dic)
+		#pprint.pprint(send)
+		#dict2= get_sequences('test.fasta',send)
+		#print(dict2)
+		#write_fatsa(dict2)
+		#convert_fatsa_nexus('filtered.fasta','output.nex')
+		#run_svd()
+		regraft(tree_,send)
 
 
